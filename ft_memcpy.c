1215ca0831cb56e24c8e118382eb9ca8e710d61a
Danie LINDE <dlinde@c4r17s6.wethinkcode.co.za>
2019-05-24 15:00:08 +0200

    mem

---
+++ ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlinde <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/24 13:38:28 by dlinde            #+#    #+#             */
+/*   Updated: 2019/05/24 13:57:16 by dlinde           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char		*s1;
+	unsigned const char	*s2;
+	size_t				i;
+
+	i = 0;
+	s1 = (unsigned char *)dst;
+	s2 = (unsigned const char *)src;
+	while (i < n)
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	return (dst);
+}
