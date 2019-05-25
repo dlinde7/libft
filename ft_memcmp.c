929ed2631e8e0477f057e30b6251b49af658045c
Danie LINDE <dlinde@c4r17s6.wethinkcode.co.za>
2019-05-25 11:32:20 +0200

    mem

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlinde <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/25 11:10:10 by dlinde            #+#    #+#             */
+/*   Updated: 2019/05/25 11:32:00 by dlinde           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t		i;
+	const char	*str1;
+	const char	*str2;
+
+	i = 0;
+	str1 = (const char *)s1;
+	str2 = (const char *)s2;
+	if (str1 == str2 || n == 0)
+		return (0);
+	while (str1[i] == str2[i] && i < n)
+		i++;
+	if (i == n)
+		return (0);
+	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+}
