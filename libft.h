46ddcf645f3c6d97847ccd04bed38f4e8336ee42
Danie LINDE <dlinde@c4r17s6.wethinkcode.co.za>
2019-05-27 10:50:42 +0200

    updated

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dlinde <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 11:12:08 by dlinde            #+#    #+#             */
-/*   Updated: 2019/05/25 13:47:56 by dlinde           ###   ########.fr       */
+/*   Updated: 2019/05/27 10:49:06 by dlinde           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,5 +58,10 @@ void	ft_striter(char *s, void (*f)(char *));
 void	ft_striteri(char *s, void(*f)(unsigned int, char *));
 int		ft_strequ(const char *s1, const char *s2);
 int		ft_strnequ(const char *s1, const char *s2, size_t n);
+void	*ft_memalloc(size_t size);
+void	ft_memdel(void **ap);
+char	*ft_strnew(size_t size);
+void	ft_strdel(char **as);
+char	*ft_strmap(char const *s, char (*f)(char));
 
 #endif
